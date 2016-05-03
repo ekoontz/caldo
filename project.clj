af130bf4f3aee338c363afd74659346edeeedb0b
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-02 20:13:56 -0700

    first working webserver

--- project.clj
+++ project.clj
@@ -2,6 +2,38 @@
   :description "the hottest new game"
   :url "https://caldo.verbcoach.com"
   :license {:name "Proprietary"
-            :url ""}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+            :url "http://verbcoach.com"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [babel "1.1.2"]
+                 [digest "1.4.4"]
+                 [environ "1.0.0"]
+                 [friend-oauth2 "0.1.3"]
+                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [http-kit "2.1.16"]
+                 [org.apache.httpcomponents/httpclient "4.3.5"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/tools.nrepl "0.2.7"]
+                 [org.postgresql/postgresql "9.4.1208.jre7"]]
+
+
+  :plugins [[cider/cider-nrepl "0.11.0"]
+            [lein-environ "1.0.0"]
+            [lein-ring "0.9.7"]
+            [s3-wagon-private "1.2.0"]]
+  :repositories {"s3" {:url "s3p://ekoontz-repo/releases/"
+                       :username :env/aws_access_key ;; gets environment variable AWS_ACCESS_KEY
+                       :passphrase :env/aws_secret_key ;; gets environment variable AWS_SECRET_KEY
+                       }}
+                 
+  :resource-paths ["resources"]
+  
+  ;; caldo.core/app is defined in src/caldo/core.clj.
+  :ring {:handler caldo.core/app})
+
+
+
 
--- src/caldo/core.clj
+++ src/caldo/core.clj
@@ -1,6 +1,53 @@
-(ns caldo.core)
+(ns caldo.core
+  (:use
+   [hiccup core page]
+   [ring.util.codec :as codec])
+  (:require
+   [caldo.auth.google :as google-auth]
+   [cemerick.friend :as friend]
+   [cemerick.friend [workflows :as workflows]]
+   [clojure.java.io :as io]
+   [clojure.tools.logging :as log]
+   [compojure.core :refer [context defroutes GET PUT POST DELETE ANY]]
+   [compojure.route :as route]
+   [compojure.handler :as handler]
+   [environ.core :refer [env]]
+   [friend-oauth2.workflow :as oauth2]
+   [hiccup.page :as h]
+   [ring.adapter.jetty :as jetty]
+   [ring.middleware.session :as session]
+   [ring.middleware.session.cookie :as cookie]
+   [ring.middleware.stacktrace :as trace]
+   [ring.util.codec :as codec]
+   [ring.util.response :as resp]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defroutes main-routes
+  (GET "/" request
+       {:status 200
+        :headers {"Content-type" "text/html;charset=utf-8"}
+        :body (html [:head [:title "benvenuto a caldo!"]
+                     :body [:div "benvenuto a caldo"]])}))
+                  
+
+(def app
+  (handler/site 
+   (friend/authenticate
+    main-routes
+
+    {:allow-anon? true
+     :login-uri "/login"
+     :default-landing-uri "/"
+     :unauthorized-handler #(-> 
+                             {:status 401
+                              :headers {"Content-type" "text/plain;charset=utf-8"}
+                              :body "Unauthorized."})
+
+     :workflows [(workflows/interactive-form)
+                 (oauth2/workflow google-auth/auth-config)]})))
+
+
