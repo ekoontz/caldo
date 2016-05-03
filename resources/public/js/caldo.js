aebe28934e8bc9723b6f96d67a87a6a66a6f31d9
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-02 23:06:52 -0700

    finished https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls

--- resources/public/js/caldo.js
+++ resources/public/js/caldo.js
@@ -9,9 +9,11 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
+var ballRadius = 10;
+
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x,y,10,0,Math.PI*2);
+    ctx.arc(x,y,ballRadius,0,Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -20,8 +22,18 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
+
+
     x += dx;
     y += dy;
+
+    if ((y + dy - ballRadius < 0) || (y + dy > (canvas.height - ballRadius))) {
+	dy = -dy;
+    }
+    if ((x + dx - ballRadius < 0) || (x + dx > canvas.width - ballRadius)) {
+	dx= -dx;
+    }
+
 }
 
 setInterval(draw,10);
