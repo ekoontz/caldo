eb6a04217ddd6ba9132cd2866ed2a4730f75e43c
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-03 10:33:34 -0700

    finish https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Build_the_brick_field

--- resources/public/js/caldo.js
+++ resources/public/js/caldo.js
@@ -2,16 +2,61 @@
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+// ball
 var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+
+// paddle
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+
+// bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+// initialize bricks
+var bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r=0; r < brickRowCount; r++) {
+	bricks[c][r] = {x: 0,
+			y: 0};
+    }
+}
+
+function drawBricks() {
+    for (c = 0; c < brickColumnCount; c++) {
+	for (r = 0; r < brickRowCount; r++) {
+	    var brickX =
+		(c * (brickWidth + brickPadding)) +
+		brickOffsetLeft;
+	    var brickY =
+		(r * (brickHeight + brickPadding)) +
+		brickOffsetTop;
+	    bricks[c][r].x = brickX;
+	    bricks[c][r].y = brickY;
+	    ctx.beginPath();
+	    ctx.rect(brickX, brickY,
+		     brickWidth,brickHeight);
+	    ctx.fillStyle = "#0095dd";
+	    ctx.fill();
+	    ctx.closePath();
+	}
+    }
+}
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -51,6 +96,7 @@ function drawPaddle() {
 
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
     x += dx;
