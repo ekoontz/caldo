d166dfdd424cc382a37c910c93c43b7b3872c0ed
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-03 14:52:38 -0700

    finished https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser/Load_the_assets_and_print_them_on_screen

--- resources/public/js/caldo.js
+++ resources/public/js/caldo.js
@@ -1,20 +1,29 @@
 // welcome to caldo!
 // note that phaser{.min}.js must be loaded for caldo.js to work.
 
-var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
-    preload: preload, create: create, update: update
-});
+var game =
+    new Phaser.Game(
+	480, 320,
+	Phaser.AUTO,
+	null, {
+	    preload: preload,
+	    create: create,
+	    update: update
+	});
+
+var ball;
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = "#edc";
+    game.load.image('ball','img/ball.png');
 }
 
 
 function create() {
-
+    ball = game.add.sprite(50,50,'ball');
 }
 
 function update() {
