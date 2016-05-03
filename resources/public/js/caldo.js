ceb208121a707d397e7793588873b16b31caeab1
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-03 11:09:41 -0700

    finished https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Collision_detection

--- resources/public/js/caldo.js
+++ resources/public/js/caldo.js
@@ -32,7 +32,8 @@ for (c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (r=0; r < brickRowCount; r++) {
 	bricks[c][r] = {x: 0,
-			y: 0};
+			y: 0,
+			status: true};
     }
 }
 
@@ -47,16 +48,34 @@ function drawBricks() {
 		brickOffsetTop;
 	    bricks[c][r].x = brickX;
 	    bricks[c][r].y = brickY;
-	    ctx.beginPath();
-	    ctx.rect(brickX, brickY,
-		     brickWidth,brickHeight);
-	    ctx.fillStyle = "#0095dd";
-	    ctx.fill();
-	    ctx.closePath();
+
+	    if (bricks[c][r].status == true) {
+		ctx.beginPath();
+		ctx.rect(brickX, brickY,
+			 brickWidth,brickHeight);
+		ctx.fillStyle = "#0095dd";
+		ctx.fill();
+		ctx.closePath();
+	    }
 	}
     }
 }
 
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+	for (r = 0; r < brickRowCount; r++) {
+	    var b = bricks[c][r];
+	    if (b.status == true &&
+		x > b.x &&
+		x < (b.x + brickWidth) &&
+		y > b.y &&
+		y < (b.y + brickHeight)) {
+		b.status = false;
+		dy = -dy;
+	    }
+	}
+    }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -99,6 +118,7 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
     x += dx;
     y += dy;
 
