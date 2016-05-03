526f159a808da3c66c4c96d5b17196af5d5d8d9f
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-02 19:36:02 -0700

    off to a great start!

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to caldo
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject caldo "0.1.0-SNAPSHOT"
+  :description "the hottest new game"
+  :url "https://caldo.verbcoach.com"
+  :license {:name "Proprietary"
+            :url ""}
+  :dependencies [[org.clojure/clojure "1.7.0"]])
+
