31ad7b930e9e34ed29badedf2588d7fb815cf081
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-04 11:21:00 -0700

    add shelf and (one) word

--- resources/public/js/caldo.js
+++ resources/public/js/caldo.js
@@ -13,6 +13,7 @@ var game =
 
 // globals
 var word;
+var shelf;
 
 // methods
 
@@ -21,19 +22,34 @@ function preload() {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = "#edc";
-    game.load.image('word','img/brick.png');
+    game.load.image('word','img/ball.png');
+    game.load.image('shelf','img/brick.png');
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    word = game.add.sprite(5,5,'word')
+    word = game.add.sprite(20,5,'word')
+    shelf = game.add.sprite(20,250,'shelf')
+
     game.physics.enable(word,Phaser.Physics.ARCADE);
-    word.body.gravity.y = 100.0;
+    word.body.gravity.y = 100.0; // word is falling.
     word.body.bounce.set(0.5);
     word.body.collideWorldBounds = true;
 
+    game.physics.enable(shelf,Phaser.Physics.ARCADE);
+    shelf.body.gravity.y = 0; // shelf is floating in the air.
+    shelf.body.bounce.set(0.5);
+    shelf.body.collideWorldBounds = true;
+    shelf.body.immovable = true;
+
+}
+
+function wordHitShelf(word,shelf) {
+    foo = 32;
 }
 
 function update() {
+    game.physics.arcade.collide(word,shelf,wordHitShelf);
 }
+
