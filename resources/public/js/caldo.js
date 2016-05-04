0919cdfb4958a8dfc30a04f9d45d64de84f899b9
Eugene Koontz <ekoontz@hiro-tan.org>
2016-05-04 08:59:18 -0700

    finish https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_breakout_game_Phaser/Extra_lives

--- resources/public/js/caldo.js
+++ resources/public/js/caldo.js
@@ -21,6 +21,10 @@ var brickInfo;
 var scoreText;
 var score = 0;
 
+var lives = 3;
+var livesText;
+var lifeLostText;
+
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
@@ -80,28 +84,51 @@ function create() {
     ball.body.collideWorldBounds = true;
     ball.body.velocity.set(125,-250);
     ball.body.bounce.set(1);
-    ball.body.gravity.y = 15;
+    ball.body.gravity.y = 0;
     game.physics.arcade.checkCollision.down = false;
 
     ball.checkWorldBounds = true;
-    ball.events.onOutOfBounds.add(function() {
-	alert('game over!');
-	location.reload();
-    },this);
+    ball.events.onOutOfBounds.add(ballLeaveScreen,this);
     
     paddle.anchor.set(0.5,1);
     paddle.body.immovable = true;
 
     initBricks();
 
-    scoreText = game.add.text(5,5,'Points: 0',
-			      {font: '18px Arial',
-			       fill: '#0095dd'});
+    textStyle = { font: '18px Arial', fill: '#0095DD' };
+    
+    scoreText = game.add.text(5,5,'Points: 0',textStyle);
     
 
     brickText = game.add.text(200,5,'Bricks: ' + bricks.children.length,
-			      {font: '18px Arial',
-			       fill: '#eddafaa'});
+			      textStyle);
+
+    livesText = game.add.text(game.world.width-5, 5, 'Lives: ' + lives,
+			      textStyle);
+    livesText.anchor.set(1,0);
+    lifeLostText = game.add.text(game.world.width*0.5,
+				 game.world.height*0.5,
+				 'Life lost, click to continue',
+				 textStyle);
+    lifeLostText.anchor.set(0.5);
+    lifeLostText.visible = false;
+}
+
+function ballLeaveScreen() {
+    lives--;
+    if (lives) {
+	livesText.setText('Lives:'+lives);
+	lifeLostText.visible = true;
+	ball.reset(game.world.width*0.5,game.world.height-25);
+	paddle.reset(game.world.width*0.5,game.world.height-5);
+	game.input.onDown.addOnce(function() {
+	    lifeLostText.visible = false;
+	    ball.body.velocity.set(250,-150);
+	},this);
+    } else {
+	alert('You lost, game over!');
+	location.reload();
+    }
 }
 
 function ballHitBrick(ball,brick) {
